e4b3726039bd272939d7ea01c450c35cded53828
Ravi-Sinhmar <engineerfuture82@gmail.com>
2025-05-07 00:15:20 +0530

    cutie will cheat me some day

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vercel from 'vite-plugin-vercel'
+import tailwindcss from '@tailwindcss/vite';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vercel()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          vendor: ['date-fns', 'lucide-react']
-        }
-      }
-    }
-  },
-  server: {
-    port: 3000
-  },
-  preview: {
-    port: 3000
-  }
+  plugins: [react(),tailwindcss()],
 })
