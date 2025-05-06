b9d1d85add58af435cd76606e36b6536d2eb7709
Ravi-Sinhmar <engineerfuture82@gmail.com>
2025-05-07 00:00:53 +0530

    resolved some error

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import vercel from 'vite-plugin-vercel'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), vercel()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          vendor: ['date-fns', 'lucide-react']
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000
+  }
 })
